ea5b01b441988f775de73190f72e984442f33e7f
Adarsh Prashar <adarsh.prashar32@gmail.com>
2022-10-26 02:36:16 +0530

    Update Missing Number.cpp

--- CSES/Introductory Problems/Missing Number.cpp
+++ CSES/Introductory Problems/Missing Number.cpp
@@ -5,43 +5,45 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-typedef long long int ll;
-typedef unsigned long long int ull;
-const ll MAXN = 2e5+10;
-const ll mod = 1000000007;
-const ll N = 200005;
- 
-void solve(){
-	int n;
-	cin>>n;
-	std::vector<int> vec(n-1);
-	for(int i=0 ; i<n-1 ; i++){
-		cin>>vec[i];
-	}	
-	sort(vec.begin(), vec.end());
-	for(int i=0 ; i<n-1 ; i++){
-		if(vec[i]!=(i+1)){
-			cout<<i+1<<"\n";
-			return;
-		}
-	}
-	cout<<n<<"\n";
-	return;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>vec(n-1);
+    vector<bool>have(n+1, false);
+    for(int i=0 ; i<n-1 ; i++){
+        cin>>vec[i];
+        have[vec[i]] = true;
+    }
+    int ans = 0;
+    for(int i=1 ; i<=n ; i++){
+        if(have[i] == false){
+            ans = i;
+            break;
+        }
+    }
+    cout<<ans<<"\n";
+    return 0;
 }
- 
-int main()
-{
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-        
-	int t = 1;
-	// cin>>t;
-	while(t--){
-		solve();
-	}
-    
-return 0;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>vec(n-1);
+    for(int i=0 ; i<n-1 ; i++){
+        cin>>vec[i];
+    }
+    int ans = 0;
+    sort(vec.begin(), vec.end());
+    for(int i=0 ; i<n-1 ; i++){
+        if(vec[i] != (i+1)){
+            ans = i+1;
+            break;
+        }
+    }
+    cout<<ans<<"\n";
+    return 0;
 }
