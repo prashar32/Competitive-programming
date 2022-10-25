e3fbb8ac925ed54f712e8d71d9a76bac1334973f
Adarsh Prashar <adarsh.prashar32@gmail.com>
2022-10-26 02:07:03 +0530

    Update Weird Algorithm.cpp

--- CSES/Introductory Problems/Weird Algorithm.cpp
+++ CSES/Introductory Problems/Weird Algorithm.cpp
@@ -5,43 +5,18 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
 typedef long long int ll;
-typedef unsigned long long int ull;
-const ll MAXN = 2e5+10;
-const ll mod = 1000000007;
-const ll N = 200005;
- 
-void solve(){
+
+int main(){
     ll n;
     cin>>n;
-    cout<<n<<" ";
-    while(n>1ll){
-        if(n&1ll){
-            n*=3ll;
-            n+=1ll;
-        }
-        else{
-            n/=2ll;
-        }
+    while(n != 1){
         cout<<n<<" ";
+        if(n&1)
+            n = 3ll*n + 1;
+        else
+            n /= 2ll;
     }
-    cout<<"\n";
-    return;
-}
- 
-int main()
-{
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-        
-    int t = 1;
-    // cin>>t;
-    while(t--){
-        solve();
-    }
-    
-return 0;
+    cout<<n<<"\n";
+    return 0;
 }
