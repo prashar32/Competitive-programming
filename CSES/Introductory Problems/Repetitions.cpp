12f1a63c86c1d5db499b593162e570b3b23e3822
Adarsh Prashar <adarsh.prashar32@gmail.com>
2022-10-27 19:42:25 +0530

    Update Repetitions.cpp

--- CSES/Introductory Problems/Repetitions.cpp
+++ CSES/Introductory Problems/Repetitions.cpp
@@ -5,43 +5,22 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-typedef long long int ll;
-typedef unsigned long long int ull;
-const ll MAXN = 2e5+10;
-const ll mod = 1000000007;
-const ll N = 200005;
- 
-void solve(){
-	string str;
-	cin>>str;
-	int n = str.size();
-	int ans = 0;
-	int cnt = 1;
-	for(int i=1 ; i<n ; i++){
-		if(str[i]==str[i-1])cnt+=1;
-		else{
-			ans = max(ans, cnt);
-			cnt = 1;
-		}
-	}
-	ans = max(ans, cnt);
-	cout<<ans<<"\n";
-	return;
-}
- 
-int main()
-{
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-        
-	int t = 1;
-	// cin>>t;
-	while(t--){
-		solve();
-	}
-    
-return 0;
+
+int main(){
+    string str;
+    cin>>str;
+    int n = str.size();
+    int cnt = 1;
+    int ans = 0;
+    for(int i=1 ; i<n ; i++){
+        if(str[i] != str[i-1]){
+            ans = max(ans, cnt);
+            cnt = 1;
+        }
+        else
+            cnt += 1;
+    }
+    ans = max(ans, cnt);
+    cout<<ans<<"\n";
+    return 0;
 }
