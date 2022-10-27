dabac9550fd997737e6efcc901b37bd7e5b3244f
Adarsh Prashar <adarsh.prashar32@gmail.com>
2022-10-27 21:32:55 +0530

    Update Increasing Array.cpp

--- CSES/Introductory Problems/Increasing Array.cpp
+++ CSES/Introductory Problems/Increasing Array.cpp
@@ -5,42 +5,19 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-typedef long long int ll;
-typedef unsigned long long int ull;
-const ll MAXN = 2e5+10;
-const ll mod = 1000000007;
-const ll N = 200005;
- 
-void solve(){
-	int n;
-	cin>>n;
-	ll ans = 0;
-	std::vector<ll> vec(n);
-	cin>>vec[0];
-	for(int i=1 ; i<n ; i++){
-		cin>>vec[i];
-		if(vec[i]<vec[i-1]){
-			ans+=(vec[i-1]-vec[i]);
-			vec[i] = vec[i-1];
-		}
-	}
-	cout<<ans<<"\n";
-	return;
-}
- 
-int main()
-{
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-        
-	int t = 1;
-	// cin>>t;
-	while(t--){
-		solve();
-	}
-    
-return 0;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>vec(n);
+    for(int i=0 ; i<n ; i++){
+        cin>>vec[i];
+    }
+    long long int ans = 0;
+    for(int i=1 ; i<n ; i++){
+        ans += max(0, vec[i-1] - vec[i]);
+        vec[i] = max(vec[i], vec[i-1]);
+    }
+    cout<<ans<<"\n";
+    return 0;
 }
